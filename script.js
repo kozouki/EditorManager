6caa302f4a3dee0cd6df8b9a33881942bc5a8305
kozouki <ventiganmatthew@gmail.com>
2023-01-01 14:09:36 +0800

    changes applies to summary

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 var add_button = document.getElementsByClassName('add');
 var minus_button = document.getElementsByClassName('minus');
 var inputs = document.getElementsByClassName('num');
+var summariesTT = document.getElementsByClassName('summaryTT');
+var summariesIG = document.getElementsByClassName('summaryIG');
 
 for(let i=0; i<add_button.length; i++){
     add_button[i].addEventListener('click', ()=>{
         var num =  parseInt(inputs[i].value);
         inputs[i].value = num+1;
+        if(i > 2){
+            summariesIG[i-summariesIG.length].innerHTML = inputs[i].value;
+            
+        }else{
+            summariesTT[i].innerHTML = inputs[i].value;
+        }
     });
 
     minus_button[i].addEventListener('click', ()=>{
@@ -14,6 +22,12 @@ for(let i=0; i<add_button.length; i++){
             inputs[i].value = 0;
         }else{
             inputs[i].value = num-1;
+        }
+
+        if(i >2){
+            summariesIG[i-summariesIG.length].innerHTML = inputs[i].value;
+        }else{
+            summariesTT[i].innerHTML = inputs[i].value;
         }
         
     });
